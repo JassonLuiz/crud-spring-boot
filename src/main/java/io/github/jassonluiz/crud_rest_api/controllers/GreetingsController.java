f7542594b7ade7b9432495cd8426c6e638b07378
JassonLuiz <jassonluizjr@hotmail.com>
2021-12-16 23:16:03 -0300

    Cria end-point de listar todos usuarios

--- src/main/java/io/github/jassonluiz/crud_rest_api/controllers/GreetingsController.java
+++ src/main/java/io/github/jassonluiz/crud_rest_api/controllers/GreetingsController.java
@@ -1,11 +1,20 @@
 package io.github.jassonluiz.crud_rest_api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.jassonluiz.crud_rest_api.model.Usuario;
+import io.github.jassonluiz.crud_rest_api.repository.UsuarioRepository;
 
 /**
  *
@@ -13,11 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GreetingsController {
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
+    
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
     @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
@@ -27,6 +35,18 @@ public class GreetingsController {
     @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	usuarioRepository.save(usuario);
+    	
     	return "Olá mundo " + nome;
+    }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
     }
 }
--- src/main/java/io/github/jassonluiz/crud_rest_api/repository/UsuarioRepository.java
+++ src/main/java/io/github/jassonluiz/crud_rest_api/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package io.github.jassonluiz.crud_rest_api.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.jassonluiz.crud_rest_api.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 }
